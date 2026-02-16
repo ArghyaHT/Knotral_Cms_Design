0fce7be6cacb08f42430d46a25ff9a92380f26df
Arghya211994 <arghya@himanstech.com>
2026-02-16 15:43:25 +0530

    routes protected for certificates

--- src/app/(cms)/certificate-list/page.js
+++ src/app/(cms)/certificate-list/page.js
@@ -1,5 +1,17 @@
+"use client"
 import CertificateList from "@/components/CertificateList/CertificateList";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
+   const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("adminAccessToken");
+
+    if (!token) {
+      router.push("/");
+    }
+  }, []);
   return <CertificateList />;
 }
